bde9676d20ffb339c08f34db61444babb8e5c550
Konstantin galiakhmetov <galiakhmetovkv@gmail.com>
2025-03-04 19:56:55 +0300

    add docker

--- config/config.go
+++ config/config.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"os"
+)
+
 type Config struct {
 	ApiKey    string
 	AccessKey string
@@ -8,12 +12,13 @@ type Config struct {
 	DBPass    string
 }
 
+// InitConfig загружает конфигурацию из переменных окружения
 func InitConfig() Config {
 	return Config{
-		ApiKey:    "$2a$10$iUWJxV84LRWQEkWZB/d/s.YMP1lCafcgp16S/7Q.dIR9BCP/Ahrvu",
-		AccessKey: "$2a$10$hhjK7nJchphzgNzBRufFg.o9nIAQps07NVIomjXzP7Q28/hEVjflW",
-		DBName:    "snaphub",
-		DBUser:    "kranid",
-		DBPass:    "lakrima54123",
+		ApiKey:    os.Getenv("API_ACCESS_KEY"),      // Загружает API_ACCESS_KEY из переменной окружения
+		AccessKey: os.Getenv("API_SECRET_KEY"),      // Загружает API_SECRET_KEY
+		DBName:    os.Getenv("MYSQL_DATABASE"),      // Загружает MYSQL_DATABASE
+		DBUser:    os.Getenv("MYSQL_USER"),          // Загружает MYSQL_USER
+		DBPass:    os.Getenv("MYSQL_ROOT_PASSWORD"), // Загружает MYSQL_ROOT_PASSWORD
 	}
 }
