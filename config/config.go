b23eb8f90850d4349344d0f2c1260cd88dfa5515
Konstantin galiakhmetov <galiakhmetovkv@gmail.com>
2025-03-05 00:57:24 +0300

    change config

--- config/config.go
+++ config/config.go
@@ -1,24 +1,24 @@
 package config
 
 import (
-	"os"
+ "os"
 )
 
 type Config struct {
-	ApiKey    string
-	AccessKey string
-	DBName    string
-	DBUser    string
-	DBPass    string
+ ApiKey    string
+ AccessKey string
+ DBName    string
+ DBUser    string
+ DBPass    string
 }
 
 // InitConfig загружает конфигурацию из переменных окружения
 func InitConfig() Config {
-	return Config{
-		ApiKey:    os.Getenv("API_ACCESS_KEY"),      // Загружает API_ACCESS_KEY из переменной окружения
-		AccessKey: os.Getenv("API_SECRET_KEY"),      // Загружает API_SECRET_KEY
-		DBName:    os.Getenv("MYSQL_DATABASE"),      // Загружает MYSQL_DATABASE
-		DBUser:    os.Getenv("MYSQL_USER"),          // Загружает MYSQL_USER
-		DBPass:    os.Getenv("MYSQL_ROOT_PASSWORD"), // Загружает MYSQL_ROOT_PASSWORD
-	}
+ return Config{
+  ApiKey:    os.Getenv("API_ACCESS_KEY"),        // Загружает API_ACCESS_KEY из переменной окружения
+  AccessKey: os.Getenv("API_SECRET_KEY"),        // Загружает API_SECRET_KEY
+  DBName:    os.Getenv("DB_NAME"),                // Загружает DB_NAME
+  DBUser:    os.Getenv("DB_USER"),                // Загружает DB_USER
+  DBPass:    os.Getenv("DB_PASS"),                // Загружает DB_PASS
+ }
 }
